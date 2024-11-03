fd7025e750d3a1e324092e89f3fd9ee6f0d6b320
vishalipar <vishalipar04@gmail.com>
2024-11-03 17:45:23 +0530

    add icons

--- vicart/settings.py
+++ vicart/settings.py
@@ -120,6 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR /'static'
+STATICFILES_DIRS = [
+    'vicart/static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
