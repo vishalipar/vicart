e10c0cc51c85cb0a91aecb6334d6671d35660a26
vishalipar <vishalipar04@gmail.com>
2024-11-20 22:19:46 +0530

    view products by category

--- store/urls.py
+++ store/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.store, name='store'),
+    path('<slug:category_slug>/', views.store, name='products_by_category'),
     path('category/', views.prod_detail, name='prod_detail'),
 ]
