fb578a911eb7132d8c25ee1787e972ec20ce487c
vishalipar <vishalipar04@gmail.com>
2024-11-21 15:58:40 +0530

    add to cart product

--- cart/models.py
+++ cart/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from product.models import Product
 # Create your models here.
 
 class Cart(models.Model):
@@ -9,3 +9,14 @@ class Cart(models.Model):
     def __str__(self):
         return self.cart_id
     
+class CartItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    is_active = models.BooleanField(default=True)
+    
+    def sub_total(self):
+        return self.product.price * self.quantity
+    
+    def __str__(self):
+        return self.product
--- cart/urls.py
+++ cart/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.cart, name='cart'),
+    path('add_cart/<int:product_id>/', views.add_cart, name='add_cart'),
     path('checkout/', views.checkout, name='checkout'),
 ]
